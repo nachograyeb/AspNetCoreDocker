5f6144d587664acefc89a750af8593fca07c31d2
nachograyeb <37944502+nachograyeb@users.noreply.github.com>
2018-08-01 05:23:29 -0300

    aspnetcoreapp files added

---
+++ aspnetcoreapp/Models/Movie.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace aspnetcoreapp.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ aspnetcoreapp/Pages/Create.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using aspnetcoreapp.Models;
+
+namespace aspnetcoreapp.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly aspnetcoreapp.Models.MovieContext _context;
+
+        public CreateModel(aspnetcoreapp.Models.MovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
